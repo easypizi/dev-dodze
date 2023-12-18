9673a23044dfba88a62e71800fa2ed25c0cc50f7
Evan Tolstof <itolstof@gmail.com>
2023-12-18 20:37:31 +0300

    update vite config

--- packages/apps/dodze/vite.config.ts
+++ packages/apps/dodze/vite.config.ts
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   envPrefix: 'DEV_DODZE',
-  base: 'dev-dodze',
   server: {
     port: 3000,
     open: true,
